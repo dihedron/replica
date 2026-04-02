5ba2a2947f8145a4b1bc4928c4aa31b7cae67a00
Andrea Funtò <dihedron.dev@gmail.com>
2026-04-02 16:54:11 +0200

    Add logging to Fetch step

--- workflow/steps.go
+++ workflow/steps.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 
 	"github.com/dbos-inc/dbos-transact-golang/dbos"
@@ -17,24 +18,35 @@ func FetchURL(url string) dbos.Step[Result[[]byte]] {
 
 		// SCENARIO A: Transient Network Error: return error to trigger retry
 		if err != nil {
+			slog.Error("error sending GET request", "url", url, "error", err)
 			return Result[[]byte]{}, fmt.Errorf("network failure: %w", err)
 		}
 		defer resp.Body.Close()
 
+		slog.Debug("GET request successfully submitted", "url", url)
+
 		// SCENARIO B: Transient Server Error (5xx): return error to trigger retry
 		if resp.StatusCode >= 500 {
+			slog.Error("internal server error", "url", url, "code", resp.StatusCode, "status", resp.Status)
 			return Result[[]byte]{}, fmt.Errorf("temporary server error (HTTP %d)", resp.StatusCode)
 		}
 
 		// SCENARIO C: Fatal Client Error (4xx): return nil tostop retrying, pass real error in Result
 		if resp.StatusCode >= 400 && resp.StatusCode < 500 {
+			slog.Error("invalid resource requested", "url", url, "code", resp.StatusCode, "status", resp.Status)
 			return Result[[]byte]{
 				Data:  nil,
 				Error: fmt.Errorf("invalid request: HTTP %d", resp.StatusCode),
 			}, nil
 		}
+		slog.Debug("response successfully received")
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			slog.Error("error reading response data", "url", url, "error", err)
+			return Result[[]byte]{}, fmt.Errorf("error reading response data (%v)", err)
+		}
 
-		body, _ := io.ReadAll(resp.Body)
+		slog.Debug("all data successfully read", "count", len(body), "data", body)
 		return Result[[]byte]{Data: body}, nil
 	}
 }
--- workflow/workflow.go
+++ workflow/workflow.go
@@ -10,10 +10,12 @@ import (
 func MainWorkflow(ctx dbos.DBOSContext, input string) (string, error) {
 	fmt.Printf("Starting workflow with input: %s\n", input)
 
-	// Execute the step durably. We can configure automatic retries here.
+	//
+	// STEP 1
+	//
 	result, err := dbos.RunAsStep(
 		ctx,
-		FetchURL("https://api.example.com/data"),
+		FetchURL("https://www.google.com"),
 		dbos.WithStepName("fetchData"),
 		dbos.WithStepMaxRetries(3),
 	)
@@ -27,6 +29,27 @@ func MainWorkflow(ctx dbos.DBOSContext, input string) (string, error) {
 	if IsFatalError(result, err) {
 		// business logic error
 		return "", fmt.Errorf("workflow aborted due to client error: %v", result.Error)
+	}
+
+	//
+	// STEP 2
+	//
+	result2, err := dbos.RunAsStep(
+		ctx,
+		SendWelcomeEmail("developer", "developer@example.com"),
+		dbos.WithStepName("sendEmail"),
+		dbos.WithStepMaxRetries(3),
+	)
+
+	// SCENARIOS A & B: retries exhausted
+	if IsRetryableError(result2, err) {
+		// run compensation logic here
+		return "", fmt.Errorf("workflow aborted: external SMTP server completely down: %w", err)
+	}
+
+	if IsFatalError(result2, err) {
+		// business logic error
+		return "", fmt.Errorf("workflow aborted due to email client error: %v", result.Error)
 	}
 
 	return fmt.Sprintf("Workflow finished successfully. Data length: %d", len(result.Data)), nil
